a25b6e29d44420baa15e30155e430855cb887235
walter <sagastegui@hotmail.com>
2021-02-15 09:33:09 +0100

    RAMA PRINCIPAL 1

---
+++ src/src/Main.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    public void mariosuma() {
+        
+    }
+    
+    public void walterresta() {
+        
+    }
+    
+    
+}
